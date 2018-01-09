00f26e3b013e52425c4fbbb77bbf6944984adae7
danielstena <32764564+danielstena@users.noreply.github.com>
2018-01-09 19:38:33 +0100

    merge

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var huvudprodukter
 var underkategorier
 
 $(document).ready(function(){
+<<<<<<< HEAD
    loadData();
 
     // Login
@@ -16,12 +17,31 @@ $(document).ready(function(){
 
     
 
+=======
+>>>>>>> parent of e71b7a8... Merge branch 'master' of https://github.com/danielstena/webshop
     $(".footer1").append("<div class='footer1left'>Supernutrition INC <br> All rights reserved</div>")
     $(".footer1").append("<div class='footer1middle'>Supernutrition INC <br> All rights reserved</div>")
     $(".footer1").append("<div class='footer1right'>Supernutrition INC <br> All rights reserved</div>")
     $(".footer2").append("<div><img src='bilder/footer-logos-se.png' class='betalning'></div>")
+<<<<<<< HEAD
     
 });
+=======
+    var json = fetch("huvudkategorier.json")
+
+    .then(function(response) {
+            return response.json();
+    })
+
+    .then(function(lista){
+            
+            var huvudprodukter = lista
+            console.log(huvudprodukter)
+
+            $(".meny").append("<a href='index.html'><button class='menybutton'>Start</button></a>")
+            $(".meny").append("<button class='menybutton'>Info</button>")
+            
+>>>>>>> parent of e71b7a8... Merge branch 'master' of https://github.com/danielstena/webshop
 
 function loadData(){ 
 
@@ -29,8 +49,15 @@ function loadData(){
         huvudkategorier = data;
         addMainList(huvudkategorier);
     });
+<<<<<<< HEAD
     
     $.getJSON('json/underkategorier.json', function(data){
+=======
+
+
+
+    $.getJSON('underkategorier.json', function(data){
+>>>>>>> parent of e71b7a8... Merge branch 'master' of https://github.com/danielstena/webshop
         underkategorier = data;
     });
     
