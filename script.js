65b8d7c2cb545bbffe698d744b7f9c6772c8433b
danielstena <32764564+danielstena@users.noreply.github.com>
2018-01-09 19:41:56 +0100

    merge

--- script.js
+++ script.js
@@ -80,5 +80,56 @@ function loadData(){
     $.getJSON('json/produkter.json', function(data){
         produkter = data;
         
+<<<<<<< HEAD
+=======
+        for(var i = 0; i < produkter.length; i++) {
+            if (produkter[i].huvudKat == 1 & produkter[i].underKat == 1 ){
+                $(".main").append("<div class='card'><div class='cardimage'><img src='" + produkter[i].prodBild + "'></div><div class='cardinfo'>" + produkter[i].prodDesc +"</div><div class='cardpris'>" + produkter[i].prodPrice + " kr</div></div>")
+                
+               
+        }}
+ 
+
+        /*
+        for(var i = 0; i < underkategorier.length; i++) {
+            if (underkategorier[i].huvudkategori == 2){
+                $(".undermenykreatin").append("<button class='undermenybutton'>"+ underkategorier[i].stadname +" </undermenybutton>")  
+        }}
+        for(var i = 0; i < underkategorier.length; i++) {
+            if (underkategorier[i].huvudkategori == 3){
+                $(".undermenypwo").append("<button class='undermenybutton'>"+ underkategorier[i].stadname +" </undermenybutton>")  
+        }}
+        for(var i = 0; i < underkategorier.length; i++) {
+            if (underkategorier[i].huvudkategori == 4){
+                $(".undermenygainer").append("<button class='undermenybutton'>"+ underkategorier[i].stadname +" </undermenybutton>")  
+        }}
+        */
+    });
+});
+
+//menyn slut//
+
+//Produkter start//
+
+/*
+
+    var json = fetch("produkter.json")
+
+    .then(function(response) {
+            return response.json();
+    })
+
+    .then(function(lista){
+            
+            var produkter = lista
+            console.log(produkter)
+
+            for(var i = 1; i < 5; i++) {
+                console.log (produkter[0].prodDesc)
+                
+                $(".mastercard").append("<div class='card'><div class='cardimage'><img src=' " + produkter[i].prodBild +  " ' class='prodbild'></div><div class='cardinfo'>Info om produkten</div><div class='cardpris'>149 kr</div><div class='buybutton'><button class='buy'>Lägg i kundkorgen</button></div></div>");
+                
+            }
+>>>>>>> parent of a4d95ee... Lagt till knapp på kort
     });
 }
